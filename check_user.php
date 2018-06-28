4c42f7901874be93c65977100abfedd3aee0a8c1
Sreenidhi-K <31153165+Sreenidhi-K@users.noreply.github.com>
2018-06-28 20:19:58 +0530

    Update check_user.php

--- check_user.php
+++ check_user.php
@@ -5,11 +5,6 @@ $dbuser="root";
 $pass="";
 $dbname="trackersite";
 $conn=mysqli_connect($host,$dbuser,$pass,$dbname);
-if(empty($_SESSION["un_ss"]))
-{
-    echo("Wrong Way !");
-    die();
-}
 $uname=mysqli_real_escape_string($conn,$_REQUEST['uname']);
 $data=mysqli_query($conn,"SELECT * FROM userslist where username='$uname'");
 if(mysqli_num_rows($data)>0)
